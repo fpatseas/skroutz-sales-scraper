e7adb38c15d6fff1fd3057216913f2d786c4b0ae
Fotis Patseas <fpatseas@users.noreply.github.com>
2020-10-19 15:55:49 +0300

    Migrated to ES6

--- src/js/chrome/background.js
+++ src/js/chrome/background.js
@@ -1,8 +1,5 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-
 chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
 		chrome.tabs.sendMessage(tabs[0].id, {type: 'history-state-updated'});
 	});
 }, {
